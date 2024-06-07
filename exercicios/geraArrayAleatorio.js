37838093f7dacbf417da9dcf7f9759484551a93b
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2024-06-07 13:21:46 -0300

    fix: corrigido problema de gerar um array com valores repetidos

--- exercicios/geraArrayAleatorio.js
+++ exercicios/geraArrayAleatorio.js
@@ -1,9 +1,8 @@
 function geraArrayAleaotorio(length, arr) {
-    for(let i = 0; i < length; i++) {
-        arr.push(Math.floor(Math.random() * 1000));
+    while(arr.length < length) {
+        if(arr.length  === length) break;
+        if(!arr[Math.floor(Math.random() * 1000)])arr.push(Math.floor(Math.random() * 1000));
     }
-
-
     return arr;
 };
 
