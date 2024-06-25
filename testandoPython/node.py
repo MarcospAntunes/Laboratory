64d41d87f9fa6aa49ccf73b8f979abb9102ab70a
Marcospantunes <marcospantunes203@gmail.com>
2024-06-25 17:59:52 -0300

    Update node.py

--- testandoPython/node.py
+++ testandoPython/node.py
@@ -2,12 +2,3 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-    
-n1 = Node(5);
-n2 = Node(2);
-n1.next = n2
-
-print(n1.data)
-print(n1.next)
-print(n2.data)
-print(n2.next)
