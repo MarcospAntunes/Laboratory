280dfae2145f47a1b3043c705b92e90c4b13fccc
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2024-07-17 07:50:18 -0300

    Imprimindo valores de um Array em java e realizando uma busca binária neste Array

--- Java/hello-world/src/App.java
+++ Java/hello-world/src/App.java
@@ -1,5 +1,21 @@
+import BinarySeach.BinarySearch;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.err.println("hello World!");
+      int[] array = {1,2,3,4,5,6,7};
+        printValues(array);
+        
+        int dir = array.length - 1;
+        int esq = 0;
+        int meio = (esq + dir) / 2;
+        
+        System.err.println(BinarySearch.find(array,0, dir, 4, meio));
+        System.err.println(BinarySearch.find(array,0, dir, 1, meio));
+        System.err.println(BinarySearch.find(array,0, dir, 7, meio));
+        System.err.println(BinarySearch.find(array,0, dir, 0, meio));
+    }
+    private static void printValues(int[] array) throws Exception {
+      for(int i = 0; i < array.length; i = i + 1) {
+        System.err.println(array[i]);
+      }
     }
 }
---
+++ Java/hello-world/src/BinarySeach/BinarySearch.java
@@ -0,0 +1,33 @@
+package BinarySeach;
+
+public class BinarySearch {
+  public static String find(int[] array, int esq, int dir, int elem, int meio) {
+    int current = (int) array[meio];
+
+    if (esq > dir) {
+      return "Elemento não encontrado";
+    }
+    
+    if (current == elem) {
+      StringBuffer sb = new StringBuffer();
+      sb.append("O item buscado esta no índice: ");
+      sb.append(meio);
+
+      String result = sb.toString();
+
+      return result;
+    } else if (current < elem) {
+      esq = meio + 1;
+      meio = (esq + dir) / 2;
+      
+      return find(array, esq, dir, elem, meio);
+    } else if (current > elem) {
+      dir = meio - 1;
+      meio = (esq + dir) / 2;
+      
+      return find(array, esq, dir, elem, meio);
+    }
+    
+    return "";
+  }
+}
